ef0749c77f1a2eab24743858dc38c406b766a1c7
JonathanSolvesProblems <jon_andrei04@hotmail.com>
2020-11-15 14:29:56 -0500

    added printing functionality to screen and receives an integer and returns the char equivalent of it.

--- main.cpp
+++ main.cpp
@@ -6,10 +6,16 @@ int main(int argc, char ** argv) {
     // sets up memory and clears the screen
     initscr();
 
+    // prints a string(const char *) to a window.
+    printw("classic hello world.");
+
     // refreshes screen to match what's in memory.
     refresh();
 
     // see what is happening, returns int value of that key.
+    int c = getch();
+    printw("%d", c);
+
     getch();
 
     endwin(); // deallocates memory and ends ncurses.
