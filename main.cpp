9d8eb8686702cb1566beae1b91491b39302503f3
JonathanSolvesProblems <jon_andrei04@hotmail.com>
2020-11-15 14:59:26 -0500

    went deeper into border customizations.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ int main(int argc, char ** argv) {
     
     initscr();
 
+    cbreak(); // allws for control-c exits out of ncurses program
+    // raw(); // doesn't accept special character. Shows us control-c in top left corner.
+    noecho(); // whatever input user types does not show on screen.
+
     int height, width, start_y, start_x;
     height = 10;
     width = 20;
@@ -13,11 +17,20 @@ int main(int argc, char ** argv) {
     WINDOW * win = newwin(height, width, start_x, start_y);
     refresh();
 
-    box(win, 0, 0);
+    char c = '+';
+
+    // box(win, (int)c, (int)c);
+    int left, right, top, bottom, tlc, trc, blc, brc;
+    left = right = 103;
+    top = bottom = 104;
+    bottom, blc = brc = (int)' ';
+    tlc = trc = (int)c;
+    wborder(win, left, right, top, bottom, tlc, trc, blc, brc);
     mvwprintw(win, 1, 1, "this is my window"); // output to window
     wrefresh(win); // only refreshes window.
 
-    int c = getch();
+    getch();
+    getch();
 
     endwin();
 
